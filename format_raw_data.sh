d682de74788794ec1efe43c7281e2d7e854754b4
Aaron <=>
2018-08-18 17:18:50 -0700

    Add initial parsing script

---
+++ format_raw_data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Extract TMK column only into separate CSV for future use
+cut -d ',' -f 2 raw.csv > tmp.csv &&
+# Move temporary file to formatted file
+cat tmp.csv > formatted.csv &&
+# Sort the TMKs in order of lowest to highest
+sort -k1 -n -t, formatted.csv > tmp.csv &&
+# Move temporary file to formatted file
+cat tmp.csv > formatted.csv &&
+# # Remove first character of each TMK (not used in the qpublic URL query param)
+cat formatted.csv | sed 's/^.//' > tmp.csv &&
+# Move temporary file to formatted file
+cat tmp.csv > formatted.csv &&
+# # Append 4 zeros to each TMK (necessary for qpublic URL query param)
+sed 's/$/0000/' formatted.csv > tmp.csv &&
+# Move temporary file to formatted file
+cat tmp.csv > formatted.csv &&
+# Remove temporary file
+rm rm tmp.csv
