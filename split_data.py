5e66e7e67d0c724fb1119e1924767ea46b06a67c
Aaron Williamson <aaron.williamson@developmentnow.com>
2018-08-20 15:43:04 -0700

    Remove unused files

--- split_data.py
+++
@@ -1,9 +0,0 @@
-with open ('formatted.csv') as f:
-    r = f.readlines()
-
-for i in range(len(r)):
-    row = r[i]
-    number = r[i].split(',')[0][0]
-    filename = "tmks_zone_"+number+".csv"
-    with open(filename,'a') as f:
-        f.write(row)
