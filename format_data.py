c4b90b73cf37bd069cc748ab8066453748cfbc68
Aaron Williamson <aaron.williamson@developmentnow.com>
2018-08-20 15:42:18 -0700

    Group TMKs by zone into separate CSV files

--- format_data.py
+++ format_data.py
@@ -2,13 +2,29 @@ import os
 import csv
 import operator
 
-# Copy the raw daya TMK column into a separate CSV with sorted TMKs
-with open('raw_example.csv') as r, open('tmk.csv', 'w') as w:
+raw_data = 'raw_example.csv'
+tmk_data = 'tmks.csv'
+tmk_zone = 'tmks_zone_'
+
+# Separate raw parcel data into sorted TMK CSV files, separated by zone
+with open(raw_data) as r, open(tmk_data, 'w') as w:
+
     # Create writer/reader objects
     writer = csv.writer(w)
     reader = csv.reader(r)
     
-    # Write each TMK value
+    # Write each TMK value to CSV, formatted w/out state code and 4-digit suffix
     sortedlist = sorted(reader, key=operator.itemgetter(1), reverse=False)
     for row in sortedlist:
-        writer.writerow([row[1]])
+        writer.writerow([row[1][1:]+'0000'])
+
+    # 
+    with open (tmk_data) as f:
+        r = f.readlines()
+
+    for i in range(len(r)):
+        row = r[i]
+        number = r[i].split(',')[0][0]
+        filename = tmk_zone + number + ".csv"
+        with open(filename,'a') as f:
+            f.write(row)
