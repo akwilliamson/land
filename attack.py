a7569871454b1b91c107f16789b5b6e071cf3209
Aaron <=>
2018-09-24 21:54:46 -0700

    Add proper parsing of taxes owed and tax year

--- attack.py
+++ attack.py
@@ -20,8 +20,19 @@ with open('records.csv', 'w') as records:
             data = urllib2.urlopen('http://qpublic9.qpublic.net/hi_hawaii_display.php?county=hi_hawaii&KEY=' + tmk[0])
             soup = BeautifulSoup(data, 'html.parser')
             tables = soup.find_all('table', attrs={'class': 'table_class'})
-            
-            tax_info = soup.find(string=re.compile("Current Tax Bill Information"))
-            table_taxes_owed = tax_info.find_parent('table')
-            raw_taxes_owed = table_taxes_owed.find(lambda tag:tag.name=="b" and "$" in tag.text)
-            print(raw_taxes_owed)
+
+            # Tax Information
+            tax_info = soup.find(string=re.compile('Current Tax Bill Information'))
+            table_tax_info = tax_info.find_parent('table')
+
+            # taxes owed
+            raw_taxes_owed = table_tax_info.find(lambda tag:tag.name=="b" and "$" in tag.text).text
+            taxes_owed = raw_taxes_owed.replace(',','').strip()[1:].strip()
+            if float(taxes_owed) == 0.00:
+                continue
+            print(taxes_owed)
+
+            # tax year
+            raw_tax_year = table_tax_info.find('td', {'class': 'sales_value'})
+            tax_year = raw_tax_year.text.strip()
+            print(tax_year)
