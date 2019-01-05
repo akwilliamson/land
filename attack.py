6a9a33da7775413539d5239d71b2b16bd36850e5
Aaron <=>
2019-01-05 12:55:00 -0800

    Add utf-8 parsing

--- attack.py
+++ attack.py
@@ -28,8 +28,8 @@ should_filter_homeowner = raw_input("do you want to filter out parcels with a 'H
 should_filter_buildings = raw_input("do you want to filter out parcels that contain 'assessed building value'? y/n\n").strip()
 # Choice to contain parcels within a specific taxable value range
 should_filter_taxable_range = raw_input("do you want to include parcels with a certain 'total taxable value' range? y/n\n").strip()
-taxable_range_minimum = ''
-taxable_range_maximum = ''
+taxable_range_minimum = '0'
+taxable_range_maximum = '1000000'
 
 if should_filter_taxable_range.lower() == 'y':
     taxable_range_minimum = raw_input("ignore parcels with a 'total taxable value' less than :\n").strip()
@@ -83,7 +83,7 @@ with open(filename, 'w') as records:
         count = 0
 
         for tmk in tmks:
-            
+            print tmk
             # get HTML for tmk record
             data = urllib2.urlopen('http://qpublic9.qpublic.net/hi_hawaii_display.php?county=hi_hawaii&KEY=' + tmk[0])
             soup = BeautifulSoup(data, 'html.parser')
@@ -96,12 +96,15 @@ with open(filename, 'w') as records:
 ### OWNER AND PARCEL INFORMATION
 
             contact_info = soup.find(string=re.compile('Owner and Parcel Information'))
+
+            if contact_info is None:
+                continue
+
             table_contact_info = contact_info.find_parent('table')
-            
             contact_values = table_contact_info.findChildren('td', attrs={'class': 'owner_value'})
 
             # acres
-            acres = contact_values[-3].string.strip()
+            acres = contact_values[-3].string.encode('utf-8').strip()
 
             raw_address = contact_values[2].text.strip()
 
@@ -111,7 +114,7 @@ with open(filename, 'w') as records:
 
             # split address's zip code
             raw_address_components = raw_address.rsplit(' ', 1)
-            zip_code = raw_address_components[-1].split('-')[0]
+            zip_code = raw_address_components[-1].split('-')[0].strip()
 
             # if the zip code is invalid, skip parsing and move to the next record
             if is_valid_zip(zip_code) == False:
@@ -119,8 +122,8 @@ with open(filename, 'w') as records:
 
             # pop zip code, split address's state abbreviation
             raw_address_components = raw_address_components[:-1]
-            state_abbreviation = raw_address_components[0].rsplit(' ', 1)[-1]
-            
+            state_abbreviation = raw_address_components[0].rsplit(' ', 1)[-1].encode('utf-8').strip()
+
             # if state abbreviation is invalid, skip parsing and move to the next record
             if state_abbreviation not in state_abbreviations:
                 continue
@@ -133,12 +136,12 @@ with open(filename, 'w') as records:
 
             # We are going to parse the most common address styles
             if re.match('PO BOX', raw_address_components[0]):
-                street = re.match('^[\bPO BOX\b+\d]+(\s)+', raw_address_components[0]).group(0)
-                city = re.split('^[\bPO BOX\b+\d]+(\s)+', raw_address_components[0])[-1].strip(',')
+                street = re.match('^[\bPO BOX\b+\d]+(\s)+', raw_address_components[0]).group(0).encode('utf-8')
+                city = re.split('^[\bPO BOX\b+\d]+(\s)+', raw_address_components[0])[-1].encode('utf-8').strip(',')
             else:
                 comps = raw_address_components[0].strip(',').rsplit(' ', 1) 
-                street = comps[0]
-                city = comps[-1]
+                street = comps[0].encode('utf-8')
+                city = comps[-1].encode('utf-8')
             
             # ## owner name
             raw_full_owner_name = contact_values[0].text.strip()
@@ -150,10 +153,10 @@ with open(filename, 'w') as records:
             first_name = ''
 
             if len(name_components) > 1:
-                last_name = name_components[0]
-                first_name = name_components[1]
+                last_name = name_components[0].encode('utf-8')
+                first_name = name_components[1].encode('utf-8')
             else:
-                first_name = name_components[0]
+                first_name = name_components[0].encode('utf-8')
 
             if (first_name.upper() == 'UNITED STATES OF AMERICA') or (first_name.upper() == 'STATE OF HAWAII'):
                 continue
@@ -179,13 +182,13 @@ with open(filename, 'w') as records:
                 continue
 
             ## assessed building value
-            assessed_building_value = assessment_values[-4].text.strip()
-            formatted_assessed_building_value = assessed_building_value.strip('$').strip()
+            assessed_building_value = assessment_values[-4].text.strip().encode('utf-8').strip()
+            formatted_assessed_building_value = assessed_building_value.strip().strip('$').strip()
             if (formatted_assessed_building_value != '0') and (should_filter_buildings.lower() == 'y'):
                 continue
 
             ## total taxable value
-            total_taxable_value = assessment_values[-1].text.strip().strip('$').strip().replace(',','')
+            total_taxable_value = assessment_values[-1].text.strip().strip('$').encode('utf-8').strip().replace(',','')
             if (float(total_taxable_value) < float(taxable_range_minimum)) or (float(total_taxable_value) > float(taxable_range_maximum)):
                 continue
 
@@ -196,13 +199,13 @@ with open(filename, 'w') as records:
 
             ## tax year
             raw_tax_year = table_tax_info.find('td', {'class': 'sales_value'})
-            tax_year = raw_tax_year.text.strip().strip('$').strip()
+            tax_year = raw_tax_year.text.strip().strip('$').encode('utf-8').strip()
             if tax_year == '0.00':
                 continue
 
             ## taxes owed
-            raw_taxes_owed = table_tax_info.find(lambda tag:tag.name=="b" and "$" in tag.text).text
-            taxes_owed = raw_taxes_owed.replace(',','').strip()[1:].strip()
+            raw_taxes_owed = table_tax_info.find(lambda tag:tag.name=="b" and "$" in tag.text).text.encode('utf-8')
+            taxes_owed = raw_taxes_owed.replace(',','').strip()[1:].encode('utf-8').strip()
             if should_filter_back_taxes.lower() == 'y':
                 if (float(taxes_owed) < float(back_tax_minimum)) or (float(taxes_owed) > float(back_tax_maximum)):
                     continue
